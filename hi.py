1d573d178f01f93f93b5cd55e186119f3486123d
Alex D <alex.diker@live.ca>
2019-02-08 11:45:37 -0500

    Update hi.py

--- hi.py
+++ hi.py
@@ -2,8 +2,12 @@ import numpy as np
 import cv2
 
 # Load an color image in grayscale
-img = cv2.imread('m5.jpg',0)
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.imshow('image',img)
+img = cv2.imread('m.jpg',0)
+
+resized = cv2.resize(img, (600,600))
+
+cv2.namedWindow('Shopping', cv2.WINDOW_NORMAL)
+cv2.imshow('Shopping',resized)
+print(img.shape)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
